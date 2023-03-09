022257c6f82a98f157652266f8f4ff34cbfa4b17
David Arcos <arcosd@gmail.com>
2023-03-09 18:28:06 -0500

    last for today, Thursday

--- routes/authors.js
+++ routes/authors.js
@@ -3,30 +3,35 @@ const router = express.Router()
 const Author = require('../models/author')
 
 //All authors route
-router.get('/', (req, res) => {
-res.render('authors/index')
+router.get('/', (req, res) =>
+{
+   res.render('authors/index')
 })
 
 //New author route
-router.get('/new', (req, res) => {
-   res.render('authors/new', {author: new Author()})
+router.get('/new', (req, res) =>
+{
+   res.render('authors/new', { author: new Author() })
 })
 
 // Create authors route
-router.post('/', (req, res) => {
+router.post('/', (req, res) =>
+{
    const author = new Author({
       name: req.body.name
-   })
-   author.save((err, newAuthor) => {
+   });
+   author.save((err, newAuthor) =>
+   {
       if (err) {
+         const errorMessage = "There was an error creating the author.";
          res.render('authors/new', {
             author: author,
-            errorMessage: "there is an error over here"
-         })
+            errorMessage: errorMessage
+         });
       } else {
-         //res.redirect(`authors/${newAuthor.id}`)
-         res.redirect( 'authors' )
+         res.redirect('/authors');
       }
-   })
-})
+   });
+});
+
 module.exports = router;
